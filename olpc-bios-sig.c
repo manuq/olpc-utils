08941e2a52650432ad3dcb511adb9478397217cd
John (J5) Palmieri <quinticent@localhost.localdomain>
2007-06-23 12:50:38 -0400

    add missing header files

--- olpc-bios-sig.c
+++ olpc-bios-sig.c
@@ -21,6 +21,11 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
 
 #define SIGNATURE_SIZE 16
 
