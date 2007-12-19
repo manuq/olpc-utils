f4fd012fd72747a8df3f80c6b22e51129d08cece
Bernardo Innocenti <bernie@codewiz.org>
2007-12-20 00:26:47 +0100

    Remove stupid 'exit 0' in zzz_olpc.sh that makes bash *exit* rather than skip the scriptlet

--- zzz_olpc.sh
+++ zzz_olpc.sh
@@ -1,20 +1,12 @@
-case "`tty`" in
+case "$LANG" in
+*.utf-8|*.UTF-8)
+	case "`tty`" in
 	not\ a\ tty)
 		# Don't print anything on non interactive sessions
-		exit 0
 	;;
-	# Not needed: /etc/profile.d/lang.sh does it for us
-	#tty[0-9])
-	#	# The linux console forgets how to output utf-8 on each logoff
-	#	/bin/unicode_start sun12x22
-	#;;
 	*)
-		# No kludges needed for remote terminals
+		cat /etc/motd.olpc 2>/dev/null
 	;;
-esac
-
-case "$LANG" in
-	*.utf-8|*.UTF-8)
-		cat /etc/motd.olpc
-	;;
+	esac
+;;
 esac
