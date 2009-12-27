18ba6170f7a07531fd9179bc2eccde69e8fd9ba5
Daniel Drake <dsd@laptop.org>
2009-12-27 11:56:25 +0000

    Tweak python optimization setting
    Set for terminal sessions too, and simplify detection method

--- etc/profile.d/zzz_olpc.sh
+++ etc/profile.d/zzz_olpc.sh
@@ -16,3 +16,9 @@ case $PATH in
 	*/sbin/*) ;;
 	*) PATH=/usr/local/sbin:/usr/sbin:/sbin:$PATH ;;
 esac
+
+# enable python optimized bytecode if the files are present (#8431)
+if [ -e /usr/lib/python2.?/subprocess.pyo ]; then
+	export PYTHONOPTIMIZE=2
+fi
+
